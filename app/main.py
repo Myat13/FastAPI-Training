36ad155db583776c251fc8953ec51f1fdd596731
Myat13 <myatminkaung1313@gmail.com>
2022-06-14 19:34:23 +0630

    CI AND CD FROM GITHUB ACTION

--- app/main.py
+++ app/main.py
@@ -19,4 +19,4 @@ app.include_router(votes.router)
 
 @app.get('/')
 def root():
-    return {"message": "Greeting from FastAPI with CI to Heroku server update agian"}
+    return {"message": "Greeting from FastAPI with CI github action to Heroku server update"}
